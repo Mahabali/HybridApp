587e10e859ed1fd1a4c0e94395895315745a64e2
Mahabali <mahabali3004@gmail.com>
2016-02-14 23:53:35 +0530

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,77 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+(function () {
+    var app = angular.module('starter', ['ionic', 'Cingo.controllers', 'Cingo.services'])
+
+    app.run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a much nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+    app.config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+                .state('signUp', {
+                    url: '/signUp',
+                    templateUrl: 'templates/signUp.html',
+                    controller: 'SignUpController'
+                })
+                .state('signIn', {
+                    url: '/signIn',
+                    templateUrl: 'templates/signIn.html',
+                    controller: 'SignInController'
+                })
+                .state('home', {
+                    url: '/',
+                    templateUrl: '/templates/signUp.html',
+                })
+                .state('tabs', {
+                    url: '/tabs',
+                    abstract: true,
+                    templateUrl: "templates/tabs.html"
+                })
+                .state('tabs.vendors', {
+                    url: '/vendors',
+                    views: {
+                        'tabs-vendors': {
+                            templateUrl: 'templates/vendors.html'
+                            
+                        }
+                    }
+                })
+
+                .state('tabs.requests', {
+                    url: '/requests',
+                    views: {
+                        'tabs-requests': {
+                            templateUrl: 'templates/requests.html'
+                            
+                        }
+                    }
+                })
+                .state('tabs.settings', {
+                    url: '/settings',
+                    views: {
+                        'tabs-settings': {
+                            templateUrl: 'templates/settings.html'
+                           
+                        }
+                    }
+                });
+        $urlRouterProvider.otherwise('/signUp');
+    });
+}());
---
+++ www/js/controllers.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+angular.module('Cingo.controllers',[])
+
+.controller('SignUpController',function($scope,$state){
+    $scope.signUp= function(user){
+        console.log("signup tapped");
+         $state.go('tabs.vendors');
+    };
+    $scope.signIn = function(){
+      $state.go('signIn');
+    };
+    
+})
+
+.controller('SignInController',function($scope,$ionicHistory){
+    $scope.signIn= function(user){
+        console.log("signIn tapped"+user.email);
+    };
+     $scope.signUp= function(user){
+        $ionicHistory.goBack();
+        console.log("signIn tapped"+user.email);
+    };
+})
